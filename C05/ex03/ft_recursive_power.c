0a1b70e1e5c8e13d1438ada99bc6a131464a4591
Pedro Manuel Branco Da Cruz <pbranco-@c1r1s3.42porto.com>
2024-09-16 11:10:23 +0100

    res

---
+++ C05/ex03/ft_recursive_power.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pbranco- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/16 11:05:30 by pbranco-          #+#    #+#             */
+/*   Updated: 2024/09/16 11:05:33 by pbranco-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power(int nb, int power)
+{
+	int	res;
+
+	res = nb;
+	if (power > 1)
+		return (nb * ft_recursive_power(nb, (power - 1)));
+	if (power == 0)
+		return (1);
+	if (power < 0)
+		return (0);
+	return (res);
+}
