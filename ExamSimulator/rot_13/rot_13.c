aab0b6df9b040b4abe1992a11ec1d4b875f385e3
Pedro Manuel Branco Da Cruz <pbranco-@c1r1s3.42porto.com>
2024-09-19 17:14:24 +0100

    res

--- ExamSimulator/rot_13/rot_13.c
+++ ExamSimulator/rot_13/rot_13.c
@@ -5,32 +5,39 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: pbranco- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/15 14:58:00 by pbranco-          #+#    #+#             */
-/*   Updated: 2024/09/15 14:58:01 by pbranco-         ###   ########.fr       */
+/*   Created: 2024/09/18 14:15:40 by pbranco-          #+#    #+#             */
+/*   Updated: 2024/09/18 14:15:41 by pbranco-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
-
-void	changechar(char *str)
-{	
+void rot(char *str)
+{
+	int i = 0;
 	char c;
-	while (*str != '\0')
+	while(str[i] != '\0')
 	{
-		if ((*str >= 'A' && *str <= 'M') || (*str >= 'a' && *str <= 'm'))
-			c = *str + 13;
-		else if ((*str >= 'M' && *str <= 'Z') || (*str >= 'm' && *str <= 'z'))
-			c = *str - 13;
-		write(1, &c, 1);
-		str++;
+		if((str[i] >= 'A' && str[i] <= 'M') || (str[i] >= 'a' && str[i] <= 'm'))
+		{
+			c = str[i] + 13;
+			write(1, &c, 1);
+		}
+		else if((str[i] > 'M' && str[i] <= 'Z') || (str[i] > 'm' && str[i] <= 'z'))
+		{
+			c = str[i] - 13;
+			write(1, &c, 1);
+		}
+		else
+		{
+			write(1, &str[i], 1);
+		}
+		i++;
 	}
 }
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	if (argc == 2)
-	{
-		changechar(argv[1]);
-	}
+	if(argc == 2)
+		rot(argv[1]);
 	write(1, "\n", 1);
 	return (0);
 }
