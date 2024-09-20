b4e69d2dcb7ca2cef498808308481abed5630cc5
Pedro Manuel Branco Da Cruz <pbranco-@c1r1s3.42porto.com>
2024-09-20 13:32:51 +0100

    res

--- ExamSimulator/rev_print/rev_print.c
+++ ExamSimulator/rev_print/rev_print.c
@@ -1,18 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_swap.c                                          :+:      :+:    :+:   */
+/*   rev_print.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pbranco- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/20 13:23:36 by pbranco-          #+#    #+#             */
-/*   Updated: 2024/09/20 13:23:37 by pbranco-         ###   ########.fr       */
+/*   Created: 2024/09/20 13:25:40 by pbranco-          #+#    #+#             */
+/*   Updated: 2024/09/20 13:25:41 by pbranco-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <unistd.h>
 
-void	ft_swap(int *a, int *b)
+int ft_strlen(char *str)
 {
-	int c = *b;
-	*b = *a;
-	*a = c;
+	int i = 0;
+	while(str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+void rev(char *str)
+{
+	int i = ft_strlen(str) - 1;
+	while(i >= 0)
+	{
+		write(1, &str[i], 1);
+		i--;
+	}
+	
+}
+
+int main(int argc, char **argv)
+{
+	if(argc == 2)
+		rev(argv[1]);
+	write(1, "\n", 1);
+	return(0);
 }
--- ExamSimulator/search_and_replace/search_and_replace.c
+++ ExamSimulator/search_and_replace/search_and_replace.c
@@ -5,24 +5,12 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: pbranco- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/19 08:49:36 by pbranco-          #+#    #+#             */
-/*   Updated: 2024/09/19 08:49:37 by pbranco-         ###   ########.fr       */
+/*   Created: 2024/09/20 13:14:01 by pbranco-          #+#    #+#             */
+/*   Updated: 2024/09/20 13:14:02 by pbranco-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <unistd.h>
 
-void replace(char *str, char a, char b)
-{
-	int i = 0;
-	while(str[i] != '\0')
-	{
-		if(str[i] == a)
-			write(1, &b, 1);
-		else
-			write(1, &str[i], 1);
-		i++;
-	}
-}
+#include <unistd.h>
 
 int ft_strlen(char *str)
 {
@@ -31,13 +19,30 @@ int ft_strlen(char *str)
 	{
 		i++;
 	}
-	return(i);
+	return (i);
 }
+
+void replace(char *str, char a, char b)
+{
+	int i = 0;
+	while(str[i] != '\0')
+	{
+		if(str[i] == a)
+		{
+			write(1, &b, 1);
+		}else
+		{
+			write(1, &str[i], 1);
+		}
+		i++;
+	}
+}
+
 
 int main(int argc, char **argv)
 {
 	if(argc == 4)
-		if ((ft_strlen(argv[2]) == 1) && (ft_strlen(argv[3]) == 1))
+		if(ft_strlen(argv[2]) == 1 && ft_strlen(argv[3]) == 1)
 			replace(argv[1], argv[2][0], argv[3][0]);
 	write(1, "\n", 1);
 	return(0);
