4e6eaf71e6efc1b6d5a8839630313b4a6ca6ce7e
Pedro Manuel Branco Da Cruz <pbranco-@c1r1s3.42porto.com>
2024-09-21 18:19:46 +0100

    res

--- ExamSimulator/Sucess/inter/inter.c
+++ ExamSimulator/Sucess/inter/inter.c
@@ -1,59 +1,66 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rotone.c                                           :+:      :+:    :+:   */
+/*   inter.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pbranco- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/21 11:19:48 by pbranco-          #+#    #+#             */
-/*   Updated: 2024/09/21 11:19:49 by pbranco-         ###   ########.fr       */
+/*   Created: 2024/09/21 11:29:42 by pbranco-          #+#    #+#             */
+/*   Updated: 2024/09/21 11:29:43 by pbranco-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void rotone(char *str)
+int ft_strlen(char *str)
 {
 	int i = 0;
-	char c;
-	while (str[i] != '\0')
+	while(str[i] != '\0')
 	{
-		if(str[i] >= 'A' && str[i] <= 'Z')
-		{
-			if(str[i] == 'Z')
-			{
-				write(1, "A", 1);
-			}
-			else
-			{
-				c = str[i] + 1;
-				write(1, &c, 1);
-			}
-		}
-		else if (str[i] >= 'a' && str[i] <= 'z')
-		{
-			if(str[i] == 'z')
-			{
-				write(1, "a", 1);
-			}
-			else
-			{
-				c = str[i] + 1;
-				write(1, &c, 1);
-			}
-		}
-		else
-		{
-			write(1, &str[i], 1);
-		}
 		i++;
 	}
+	return(i);
+}
+
+void inter(char *str1, char *str2)
+{
+	int i = 0;
+	int j = 1;
+
+	while(str1[i] != '\0')
+	{
+		j = i + 1;
+		while(str1[j] != '\0' && ft_strlen(str1) >= j){
+			if(str1[j] == str1[i])
+			{
+				str1[j] = 35;
+			}
+			j++;
+		}	
+		i++;
+	}
+	i = 0;
+	j = 0;
+	while(str1[i] != '\0')
+	{
+		while(str2[j] != '\0'){
+			if(str2[j] == str1[i] && j >= i && str1[i] != 35)
+			{
+				write(1, &str1[i], 1);
+				break;
+			}
+			j++;
+		}
+		j = 0;	
+		i++;
+	}
+
 }
 
 int main(int argc, char **argv)
 {
-	if(argc == 2)
-		rotone(argv[1]);
+	if(argc == 3)
+		inter(argv[1], argv[2]);
 	write(1, "\n", 1);
-	return (0);
+	return(0);
 }
--- ExamSimulator/Sucess/ulstr/ulstr.c
+++ ExamSimulator/Sucess/ulstr/ulstr.c
@@ -1,26 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   alpha.c                                            :+:      :+:    :+:   */
+/*   ulstr.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pbranco- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/20 09:09:08 by pbranco-          #+#    #+#             */
-/*   Updated: 2024/09/20 09:09:09 by pbranco-         ###   ########.fr       */
+/*   Created: 2024/09/21 11:13:33 by pbranco-          #+#    #+#             */
+/*   Updated: 2024/09/21 11:13:34 by pbranco-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 
-void cases(char *str)
+void reverse(char *str)
 {
 	int i = 0;
 	char c;
-	while(str[i] != '\0')
+	while (str[i] != '\0')
 	{
 		if(str[i] >= 'A' && str[i] <= 'Z')
-		{	
+		{
 			c = str[i] + 32;
-			write(1, &c, 1);	
+			write(1, &c, 1);
+		
 		}
 		else if(str[i] >= 'a' && str[i] <= 'z')
 		{
@@ -33,13 +34,12 @@ void cases(char *str)
 		}
 		i++;
 	}
-	
 }
 
 int main(int argc, char **argv)
 {
 	if(argc == 2)
-		cases(argv[1]);
+		reverse(argv[1]);
 	write(1, "\n", 1);
-	return (0);
+	return(0);
 }
