312094173c5a99ef9e8715187d26fee96ac147fc
Pedro Manuel Branco Da Cruz <pbranco-@c1r1s3.42porto.com>
2024-09-23 18:50:53 +0100

    res

--- ExamSimulator/Sucess/ft_atoi/ft_atoi.c
+++ ExamSimulator/Sucess/ft_atoi/ft_atoi.c
@@ -5,40 +5,39 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: pbranco- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/19 09:56:00 by pbranco-          #+#    #+#             */
-/*   Updated: 2024/09/19 09:56:01 by pbranco-         ###   ########.fr       */
+/*   Created: 2024/09/23 18:31:56 by pbranco-          #+#    #+#             */
+/*   Updated: 2024/09/23 18:31:58 by pbranco-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 
 int	ft_atoi(const char *str)
 {
+
 	int i = 0;
-	int j = 0;
 	int signal = 1;
-	while(str[i] != '\0')
+	int count = 0;
+	
+	while(str[i] != 0 &&  str[i] <= 32)
 	{
-		if(str[i] >= 43 && str[i] <= 57 && (str[i] != 44 && str[i] != 46 && str[i] != 47) )
-		{
-			if(str[i] == 45 || str[i] == 43)
-			{
-				if (str[i] == 45)
-					signal *= -1;
-			}
-			else
-			{
-				j *= 10;
-				j += str[i] - 48;
-			}
-			if (!(str[i + 1] >= 48 && str[i + 1] <=57))
-				break;
-		}
 		i++;
 	}
-	j = j* signal;
-	return(j);
+	while(str[i] >= 43 && str[i] < 48 && str[i] != 44 && str[i] != 46 && str[i] != 47)
+	{
+		if(str[i] == 45)
+			signal *= -1;
+		i++;
+	}
+	while(str[i] >= 48 && str[i] <= 57)
+	{
+		count *= 10;
+		count += str[i] - 48;
+		i++;
+	}
+	count *= signal;
+	return (count);
 }
 /*
 int main(void){
