1ee0aaefe234f8b1f93826a14b2ededb7a936d15
Pedro Manuel Branco Da Cruz <pbranco-@c1r10s1.42porto.com>
2024-12-09 14:50:08 +0000

    res

--- CommonCore/Milestone2/push_swap/src/main.c
+++ CommonCore/Milestone2/push_swap/src/main.c
@@ -6,9 +6,74 @@
 /*   By: pbranco- <pbranco-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 15:28:09 by pcruz             #+#    #+#             */
-/*   Updated: 2024/12/09 14:38:35 by pbranco-         ###   ########.fr       */
+/*   Updated: 2024/12/09 14:42:32 by pbranco-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
+void radix_sort(int *a, int size_a, int *b, int size_b)
+{
+    while (size_b > 0)
+    {
+        if (a[0] < b[0])
+        {
+            pa_pb(&b, &size_b, &a, &size_a);
+            ra_rb(b, size_b);
+        }
+        else
+        {
+            ra_rb(a, size_a);
+        }
+    }
+
+    while (size_b <= size_a)
+    {
+        if (a[0] < b[0])
+        {
+            pa_pb(&b, &size_b, &a, &size_a);
+        }
+        ra_rb(b, size_b);
+    }
+
+    while (size_b <= size_a)
+    {
+        pa_pb(&b, &size_b, &a, &size_a);
+        ra_rb(b, size_b);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    int *stack_a;
+    int *stack_b;
+    int size_a;
+    int size_b;
+    int i;
+
+    if (argc < 2)
+    {
+        ft_printf("Error: Not enough arguments.\n");
+        return 1;
+    }
+
+    size_a = argc - 1;
+    size_b = 0;
+    stack_a = ft_calloc(size_a, sizeof(int));
+    stack_b = ft_calloc(size_a, sizeof(int)); // Allocate same size for stack_b initially
+
+    i = 0;
+    while (i < size_a)
+    {
+        stack_a[i] = ft_atoi(argv[i + 1]);
+        i++;
+    }
+
+    // Apply radix sort
+    radix_sort(stack_a, size_a, stack_b, size_b);
+
+    // Clean up and exit
+    free(stack_a);
+    free(stack_b);
+    return 0;
+}
--- CommonCore/Milestone2/push_swap/src/rotate.c
+++ CommonCore/Milestone2/push_swap/src/rotate.c
@@ -6,7 +6,7 @@
 /*   By: pbranco- <pbranco-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 10:11:02 by pcruz             #+#    #+#             */
-/*   Updated: 2024/12/09 14:38:06 by pbranco-         ###   ########.fr       */
+/*   Updated: 2024/12/09 14:42:54 by pbranco-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ void ra_rb(int *arr, int size)
 
 void rr(int *a, int size_a, int *b, int size_b)
 {
-    shift_bottom_to_top(a, size_a);
-    shift_bottom_to_top(b, size_b);
+    ra_rb(a, size_a);
+    ra_rb(b, size_b);
 }
 
