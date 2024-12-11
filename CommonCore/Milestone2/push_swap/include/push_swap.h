9db3febee9daf84b4157297393acb23a8e9c7813
Pedro Manuel Branco Da Cruz <pbranco-@c1r10s1.42porto.com>
2024-12-11 10:10:15 +0000

    res

--- CommonCore/Milestone2/push_swap/include/push_swap.h
+++ CommonCore/Milestone2/push_swap/include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: pbranco- <pbranco-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 15:28:32 by pcruz             #+#    #+#             */
-/*   Updated: 2024/12/11 09:59:07 by pbranco-         ###   ########.fr       */
+/*   Updated: 2024/12/11 10:09:10 by pbranco-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +19,23 @@ typedef struct s_list
 {
     int             value;
     int             index;
-    struct s_list   *next;
+    struct t_list   *next;
 }               t_list;
 
-void	ss(t_list **stack_a, t_list **stack_b)
-void	sb(t_list **head)
-void	sa(t_list **head)
-void	ra(t_list **head)
-void	rb(t_list **head)
-void	rr(t_list **stack_a, t_list **stack_b)
-void	rra(t_list **head)
-void	rrb(t_list **head)
-void	rrr(t_list **stack_a, t_list **stack_b)
-void	push(t_list **head, int new_data)
-void	free_stack(t_list **head)
-void	pa(t_list **stack_a, t_list **stack_b)
-void	pb(t_list **stack_a, t_list **stack_b)
-int		pop(t_list **head)
-int		stack_size(t_list *head)
+void	ss(t_list **stack_a, t_list **stack_b);
+void	sb(t_list **head);
+void	sa(t_list **head);
+void	ra(t_list **head);
+void	rb(t_list **head);
+void	rr(t_list **stack_a, t_list **stack_b);
+void	rra(t_list **head);
+void	rrb(t_list **head);
+void	rrr(t_list **stack_a, t_list **stack_b);
+void	push(t_list **head, int new_data);
+void	free_stack(t_list **head);
+void	pa(t_list **stack_a, t_list **stack_b);
+void	pb(t_list **stack_a, t_list **stack_b);
+int		pop(t_list **head);
+int		stack_size(t_list *head);
 
 #endif
--- CommonCore/Milestone2/push_swap/src/main.c
+++ CommonCore/Milestone2/push_swap/src/main.c
@@ -5,15 +5,81 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: pbranco- <pbranco-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/03 15:28:09 by pcruz             #+#    #+#             */
-/*   Updated: 2024/12/11 10:00:18 by pbranco-         ###   ########.fr       */
+/*   Created: 2024/12/11 10:10:48 by pbranco-          #+#    #+#             */
+/*   Updated: 2024/12/11 10:08:18 by pbranco-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "../include/push_swap.h"
+
+void assign_indices(t_list *stack)
+{
+    t_list *current;
+    t_list *compare;
+    int index;
+
+    current = stack;
+    while (current)
+    {
+        index = 0;
+        compare = stack;
+        while (compare)
+        {
+            if (current->value > compare->value)
+                index++;
+            compare = compare->next;
+        }
+        current->index = index;
+        current = current->next;
+    }
+}
+
+void radix_sort(t_list **stack_a, t_list **stack_b)
+{
+    int max_num, max_bits, i, j, size;
+    t_list *current;
+
+    size = stack_size(*stack_a);
+    max_num = size - 1;
+    max_bits = 0;
+    while ((max_num >> max_bits) != 0)
+        max_bits++;
+
+    for (i = 0; i < max_bits; i++)
+    {
+        for (j = 0; j < size; j++)
+        {
+            current = *stack_a;
+            if (((current->index >> i) & 1) == 1)
+                ra(stack_a);
+            else
+                pb(stack_a, stack_b);
+        }
+        while (stack_size(*stack_b) != 0)
+            pa(stack_a, stack_b);
+    }
+}
 
 int main(int argc, char **argv)
 {
-    
+    t_list *stack_a = NULL;
+    t_list *stack_b = NULL;
+    int i;
+
+    if (argc < 2)
+        return (1);
+    for (i = 1; i < argc; i++)
+        push(&stack_a, ft_atoi(argv[i]));
+
+    assign_indices(stack_a);
+    radix_sort(&stack_a, &stack_b);
+
+    // Print the sorted stack
+    while (stack_a)
+    {
+        printf("%d\n", stack_a->value);
+        stack_a = stack_a->next;
+    }
+
+    free_stack(&stack_a);
     return (0);
 }
